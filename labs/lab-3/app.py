0567cc8f14a3dcf4c6b04e9f5cf9fb90937a8dfb
djgd27 <david.galindodelgado1@marist.edu>
2024-09-18 11:46:01 -0400

    debbuging html site -current status 'template not found'-

--- labs/lab-3/app.py
+++ labs/lab-3/app.py
@@ -1,5 +1,8 @@
 """app.py: render and route to webpages"""
 from db.db import *
+import os
+
+print("Current working directory:", os.getcwd())
 
 # create a webpage based off of the html in templates/index.html
 # not required: if you want to play around with some html, feel free
@@ -7,6 +10,9 @@ from db.db import *
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
