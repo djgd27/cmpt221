dc2deab1f9e4547d1d7981e828598984f990975f
djgd27 <david.galindodelgado1@marist.edu>
2024-09-24 19:32:11 -0400

    storing changes

--- labs/lab-3/app.py
+++ labs/lab-3/app.py
@@ -1,18 +1,14 @@
 """app.py: render and route to webpages"""
 from db.db import *
-import os
 
-print("Current working directory:", os.getcwd())
-
+# Define a constant for the full path to the index.html
 # create a webpage based off of the html in templates/index.html
-# not required: if you want to play around with some html, feel free
-# to edit templates/index.html
+
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'))
+
 @app.route('/')
 def index():
     return render_template("index.html")
-@app.route('/test')
-def test():
-    return render_template("test.html")
 
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
