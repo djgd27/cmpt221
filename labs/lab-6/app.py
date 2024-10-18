3c2a09de635cdb02e59c2969dd70b7902873a9b8
djgd27 <david.galindodelgado1@marist.edu>
2024-10-17 20:45:31 -0400

    finished lab 6

--- labs/lab-6/app.py
+++ labs/lab-6/app.py
@@ -35,8 +35,29 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/login')
+# +
+@app.route('/login', methods=['GET', 'POST'])  # +
 def login():
+    if request.method == 'POST':
+
+        try:
+
+            username = request.form.get("email")
+            password = request.form.get("password")
+
+            db_password = db.session.execute(text("""SELECT "Password" FROM "Users" WHERE "Email" = :username"""), {
+                "username": username}).fetchone()
+
+            if db_password is None:
+                return render_template('fail.html')
+
+            if db_password[0] == password:
+                print("Login Successful")
+                return render_template('success.html')
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
     return render_template('login.html')
 
 
